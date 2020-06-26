13231ff655febaa1324261f428d26428bbf128de
Jim Browning <jamalebrowning@gmail.com>
2020-06-25 21:42:20 -0500

    created authorization for login and nav bar.

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,12 @@
-import 'bootstrap';
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+
+import auth from './components/auth/auth';
 import '../styles/main.scss';
 
-console.error('hello');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseConfig);
+  auth.loginButton();
+};
+
+init();
